ee7bbc2432bacd15b4ddb4f194eee9e164f78ec2
Jim <jim_king_2000@163.com>
2019-11-01 20:29:37 +0800

    refactory

--- src/CanvasReactor.js
+++ src/CanvasReactor.js
@@ -6,14 +6,18 @@ export class CanvasReactor extends Component {
   componentDidUpdate() {
     if (!this.props.setFitView) return;
     const mapView = this.props.__map__.MapView;
-    setTimeout(() =>mapView.setFitView(this.props.positions), 100);
-    if (this.props.onUpdateEnd) this.props.onUpdateEnd();
+    if (!mapView) return;
+    setTimeout(() => {
+      mapView.setFitView(this.props.positions);
+      if (this.props.onUpdateEnd) this.props.onUpdateEnd();
+    }, 100);
   }
 
   render() {
     if (!this.props.tracingMode) return null;
 
     const mapView = this.props.__map__.MapView;
+    if (!mapView) return null;
     const positions = this.props.positions;
     const isMarkersInViewport = mapView.isInView(positions);
     if (!isMarkersInViewport) {
